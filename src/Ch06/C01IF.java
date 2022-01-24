e73d4cb80d2acfc00c83dccff7a84b416e9da178
jungwoogyun <ldjgood12@gmail.com>
2022-01-24 17:24:00 +0900

    2022-01-24-C3

---
+++ src/Ch06/C01IF.java
@@ -0,0 +1,34 @@
+package Ch06;
+
+import java.util.Scanner;
+
+public class C01IF {
+
+	public static void main(String[] args) {
+		
+		//단순 IF
+
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("입력 : " );
+//		int num = sc.nextInt();
+//		
+//		if(num%2==0) 	//짝수인가?
+//		{
+//			//참인경우 실행 코드블럭
+//			System.out.println(num+" 은 짝수입니다");
+//		}
+//		System.out.println("프로그램을 종료합니다");
+		
+		//정수 하나 입력받아서 3의배수이면 'n은 3의 배수입니다' 출력
+		//3의 배수이면서 4의 배수이면 'n은 3의배수이면서 4의배수입니다'출력(조건식&&조건식)
+		//두수를 입력받아 큰수를 출력 if문으로만 해결
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
